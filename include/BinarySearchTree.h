f4ed8efbd07f8357ae0626904fa9bfa94d154142
magestik <bastien@magestik.fr>
2017-01-15 23:56:20 +0100

    improved BST

--- include/BinarySearchTree.h
+++ include/BinarySearchTree.h
@@ -1,101 +1,40 @@
-/*
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
-
 #pragma once
 
 template <typename T>
 class BinarySearchTree
 {
 
-public:
-
-	struct tree_node
+	struct sNode
 	{
+		sNode(const T & d) : data(d), left(nullptr), right(nullptr) { /* ... */ }
+
 		T data;
-		tree_node * left;
-		tree_node * right;
+		sNode * left;
+		sNode * right;
 	};
 
-	BinarySearchTree(void) : root(NULL) { }
+public:
 
-	~BinarySearchTree(void)
-	{
-		// TODO free :
-		/*
-		t_ABR fg, fd;
-		if (arbre != NULL){
-			fg = arbre->f_gauche;
-			fd = arbre->f_droit;
 
-			free(arbre);
+	// Constructors / Destructor
+	BinarySearchTree(void);
+	~BinarySearchTree(void);
 
-			arbre = NULL;
+	// Add/Remove methods
+	bool insert(const T & p);
 
-			if(fg != NULL) {
-				free_arbre(fg);
-			}
-
-			if(fd != NULL) {
-				free_arbre(fd);
-			}
-		}*/
-	}
-
-	bool isEmpty(void)
-	{
-		return (NULL == this->root);
-	}
-
-	/*
-	 * Ajoute une configuration dans un ABR s'il n'est pas deja present
-	 * Renvoi 1 si l'ajout est effectué
-	 * Renvoi 0 si l'ajout n'est pas effectué
-	 */
-	bool add(const T & p)
-	{
-		return this->add(&(this->root), p);
-	}
+	// Size accessors
+	bool empty(void);
 
 private:
 
-	bool add(tree_node ** node, const T & p)
-	{
-		if (NULL == *node) {
+	bool _insert(sNode ** node, const T & p);
+	bool _remove(sNode * node);
 
-			*node = new tree_node;
+private:
 
-			(*node)->left	= NULL;
-			(*node)->right	= NULL;
-			(*node)->data = p;
+	sNode * m_pRoot;
 
-			return true;
-
-		} else {
-
-			int comparaison = compare(p, (*node)->data);
-
-			if (comparaison == 0) {
-				return false;
-			} else if (comparaison == 1) {
-				return this->add(&((*node)->right), p);
-			} else {
-				return this->add(&((*node)->left), p);
-			}
-		}
-	}
-
-
-	tree_node * root;
 };
+
+#include "BinarySearchTree.inl"
---
+++ include/BinarySearchTree.inl
@@ -0,0 +1,98 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+#ifndef ASSERT
+#	define ASSERT(exp, msg)
+#endif // ASSERT
+
+/**
+ * Default constructor
+ */
+template<typename T>
+inline BinarySearchTree<T>::BinarySearchTree(void) : m_pRoot(nullptr)
+{
+	// ...
+}
+
+/**
+ * Destructor
+ */
+template<typename T>
+inline BinarySearchTree<T>::~BinarySearchTree(void)
+{
+	_remove(m_pRoot);
+}
+
+/**
+ * Test whether the tree is empty
+ */
+template<typename T>
+inline bool BinarySearchTree<T>::empty(void)
+{
+	return (nullptr == m_pRoot);
+}
+
+/**
+ * Insert an element in the tree
+ */
+template<typename T>
+inline bool BinarySearchTree<T>::insert(const T & p)
+{
+	return(_insert(&(m_pRoot), p));
+}
+
+/**
+ * Insert an element in the tree (internal)
+ */
+template<typename T>
+inline bool BinarySearchTree<T>::_insert(sNode ** pNode, const T & p)
+{
+	if (nullptr == *pNode)
+	{
+		*pNode = new sNode(p);
+		return(true);
+	}
+	else
+	{
+		int comparaison = compare(p, (*pNode)->data);
+
+		if (comparaison == 0)
+		{
+			return(false);
+		}
+		else if (comparaison == 1)
+		{
+			return(_insert(&((*pNode)->right), p));
+		}
+		else
+		{
+			return(_insert(&((*pNode)->left), p));
+		}
+	}
+}
+
+/**
+ * Remove an element from the tree (internal)
+ */
+template<typename T>
+inline bool BinarySearchTree<T>::_remove(sNode * pNode)
+{
+	if (nullptr != pNode)
+	{
+		_remove(pNode->left);
+		_remove(pNode->right);
+		delete pNode;
+	}
+}
