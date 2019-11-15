35d3acd2e2124cd64086e7b418645860fa574bf7
Bastien <bastien@magestik.fr>
2019-11-15 17:06:48 +0100

    Enable dashboard submission

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "containers")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "dashboard.magestik.fr")
+set(CTEST_DROP_LOCATION "/submit.php?project=containers")
+set(CTEST_DROP_SITE_CDASH TRUE)
